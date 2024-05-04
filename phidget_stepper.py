cc819003b61dc4054a27a0d8163484f4341c65df
Mowibox <ousmanthiongane17@gmail.com>
2024-05-04 22:08:01 +0200

    Updating move_forward

--- phidget_stepper.py
+++ phidget_stepper.py
@@ -4,6 +4,10 @@ from robot_config import *
 import time
 import numpy as np
 
+WHEEL_RADIUS = 41.436
+ENTRAXE = 240
+speed = 3
+speed_factor = 1.0
 
 def stepper_init(serial_number, hub_port, rescale_factor=1/3200):
 	stepper = Stepper()
@@ -20,15 +24,39 @@ def stepper_init(serial_number, hub_port, rescale_factor=1/3200):
 
 def move_forward(stepper_left, stepper_right, distance):
 	step = distance/(2*np.pi*WHEEL_RADIUS)
+	position = stepper_left.getPosition()
+	stepper_left.setVelocityLimit(speed*speed_factor)
+	stepper_right.setVelocityLimit(speed*speed_factor)
 	stepper_left.setTargetPosition(step)
 	stepper_right.setTargetPosition(-step)
+	while abs(distance/260 - stepper_left.getPosition()) >= 1e-2:
+		print(stepper_left.getPosition())
+		time.sleep(0.1)
+	stepper_left.addPositionOffset(-stepper_left.getPosition())
+	stepper_right.addPositionOffset(-stepper_right.getPosition())
+	stepper_left.setVelocityLimit(0)
+	stepper_right.setVelocityLimit(0)
 	return
 	
+
+
+def rotate_left(stepper_left, stepper_right, theta):
+	distance = np.pi*ENTRAXE*theta/360
+	stepper_left.setVelocityLimit(speed*speed_factor)
+	stepper_right.setVelocityLimit(speed*speed_factor)
+	step = distance/(2*np.pi*WHEEL_RADIUS)
+	stepper_left.setTargetPosition(step)
+	stepper_right.setTargetPosition(step)
+	time.sleep(speed/distance)
+	stepper_left.setVelocityLimit(0)
+	stepper_right.setVelocityLimit(0)
+	return
+
 
 stepper_left = stepper_init(723793, 5)
 stepper_right = stepper_init(723793, 0)
 
-
+rotate_left(stepper_left, stepper_right, 90)
 
 try:
 	input("Press Enter to Stop\n")

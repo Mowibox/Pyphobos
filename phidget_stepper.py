aa689cc987af59fbdb844c0632d79c7887ddb557
Mowibox <ousmanethiongane17@gmail.com>
2024-05-04 19:25:14 +0200

    Adding move_forward

--- phidget_stepper.py
+++ phidget_stepper.py
@@ -1,6 +1,8 @@
 from Phidget22.Phidget import *
 from Phidget22.Devices.Stepper import *
+from robot_config import *
 import time
+import numpy as np
 
 
 def stepper_init(serial_number, hub_port, rescale_factor=1/3200):
@@ -11,15 +13,22 @@ def stepper_init(serial_number, hub_port, rescale_factor=1/3200):
 	stepper.setRescaleFactor(rescale_factor)
 	stepper.setControlMode(StepperControlMode.CONTROL_MODE_STEP)
 	stepper.setCurrentLimit(3)
+	stepper.setEngaged(True)
+
 	return stepper
+
+
+def move_forward(stepper_left, stepper_right, distance):
+	step = distance/(2*np.pi*WHEEL_RADIUS)
+	stepper_left.setTargetPosition(step)
+	stepper_right.setTargetPosition(-step)
+	return
+	
 
 stepper_left = stepper_init(723793, 5)
 stepper_right = stepper_init(723793, 0)
 
-stepper_left.setTargetPosition(1)
-stepper_right.setTargetPosition(-1)
-stepper_left.setEngaged(True)
-stepper_right.setEngaged(True)
+
 
 try:
 	input("Press Enter to Stop\n")

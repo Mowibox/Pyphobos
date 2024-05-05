be220cc151ed4788669db2689b88e99e9efc9151
Mowibox <ousmanethiongane17@gmail.com>
2024-05-05 23:16:45 +0200

    Indent correction

--- main_phobos.py
+++ main_phobos.py
@@ -13,14 +13,6 @@ from strategy import *
 
 checkout_led = 11
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(checkout_led, GPIO.OUT)
-for i in range(5):
-        GPIO.output(checkout_led, GPIO.HIGH)
-        time.sleep(0.2) 
-
-        GPIO.output(checkout_led, GPIO.LOW)
-        time.sleep(0.2)  
 
 def stop_program():
     time.sleep(90)  
@@ -37,39 +29,48 @@ def find_screen_port():
     else:
         return None
 
-
-port = find_screen_port() 
-baudrate = 115200  
-timeout = 1
-
-if port:
-    ser = serial.Serial(port, baudrate, timeout=timeout)
-else: 
-    print("No screen device found on target !")
-    exit()
-
-while True:
-    received_data = ser.readline().decode().strip()
-    if len(received_data) != 0:
-        strategy_number = int(received_data)
-        break
-ser.close()
-
-stepper_left = stepper_init(serial_number, left_hub_port)
-stepper_right = stepper_init(serial_number, right_hub_port)
-
-GPIO.setup(start_pin, GPIO.IN)
-previous_state = GPIO.input(start_pin)
-
-while True:
-    current_state = GPIO.input(start_pin)
-    
-    if current_state != previous_state:
-        # print(strategy)
-        break
-    time.sleep(0.1)
-
 try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(checkout_led, GPIO.OUT)
+    for i in range(5):
+            GPIO.output(checkout_led, GPIO.HIGH)
+            time.sleep(0.2) 
+
+            GPIO.output(checkout_led, GPIO.LOW)
+            time.sleep(0.2)  
+
+
+    port = find_screen_port() 
+    baudrate = 115200  
+    timeout = 1
+
+    if port:
+        ser = serial.Serial(port, baudrate, timeout=timeout)
+    else: 
+        print("No screen device found on target !")
+        exit()
+
+    while True:
+        received_data = ser.readline().decode().strip()
+        if len(received_data) != 0:
+            strategy_number = int(received_data)
+            break
+    ser.close()
+
+    stepper_left = stepper_init(serial_number, left_hub_port)
+    stepper_right = stepper_init(serial_number, right_hub_port)
+
+    GPIO.setup(start_pin, GPIO.IN)
+    previous_state = GPIO.input(start_pin)
+
+    while True:
+        current_state = GPIO.input(start_pin)
+        
+        if current_state != previous_state:
+            # print(strategy)
+            break
+        time.sleep(0.1)
+
     ser1 = serial.Serial("/dev/ttyAMA1", baudrate=115200, timeout=5.0)
     lidar_data = AresLidar()
 

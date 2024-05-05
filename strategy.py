a059b787d87dd970be1967e2e64ea3af5038784e
Mowibox <ousmanethiongane17@gmail.com>
2024-05-06 00:21:06 +0200

    Adding Strategy 0

--- strategy.py
+++ strategy.py
@@ -10,7 +10,34 @@ from phidget_stepper import *
 
 def strategy(strategy_number, stepper_left, stepper_right, lidar_data, serial):
     if strategy_number == 0:
-        pass
+        speed = 1
+        move_forward(stepper_left, stepper_right,
+         lidar_data, serial, speed, 700)
+        time.sleep(1)
+        speed = 2
+        rotate_left(stepper_left, stepper_right, speed, 90)
+        time.sleep(0.5)
+        speed = 1
+        move_forward(stepper_left, stepper_right,
+         lidar_data, serial, speed, -700)
+        speed = 2
+        rotate_left(stepper_left, stepper_right, speed, -270)
+        time.sleep(0.5)
+        speed = 1
+        move_forward(stepper_left, stepper_right,
+         lidar_data, serial, speed, -700)
+        time.sleep(1)
+        speed = 2
+        rotate_left(stepper_left, stepper_right, speed, -135)
+        time.sleep(0.5)
+        move_forward(stepper_left, stepper_right,
+         lidar_data, serial, speed, np.sqrt(2)*700)
+        time.sleep(1)
+        rotate_left(stepper_left, stepper_right, speed, -45)
+        time.sleep(0.5)
+        speed = 1
+        move_forward(stepper_left, stepper_right,
+         lidar_data, serial, speed, -700)
     elif strategy_number == 1:
         pass
     elif strategy_number == 2:
